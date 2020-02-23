49a54061d9338067639ef548fad332cf95a018bf
exzolink <exzolink@gmail.com>
2020-02-23 11:25:49 +0300

    final

--- portfolio/selitra/send.php
+++ portfolio/selitra/send.php
@@ -4,18 +4,18 @@
     $text_comment = $_POST["user_comment"];
 
         if ($name=="" or $phone=="" or $text_comment==""){ 
-            echo "Заполните все поля";
+            echo "Заполните<br>все поля";
         }
 
         else{
         $to = "selitra.biz.ua@gmail.com";
-        $subject = "Письмо с обратной связи";
+        $subject = "Заказ звонка Селiтра";
         $headers = "MIME-Version: 1.0\r\n";
         $headers .= "Content-type: text/html; charset=utf-8\r\n";
         $headers .= "From: <selitra.biz.ua@gmail.com>\r\n";
-        $message .= "Имя пользователя: ".$name."\n";
-        $message .= "Телефон: ".$phone."\n";
-        $message .= "Сообщение: ".$text_comment."\n";
+        $message .= "Имя пользователя: ".$name."<br>";
+        $message .= "Телефон: ".$phone."<br>";
+        $message .= "Сообщение: ".$text_comment."<br>";
         $send = mail($to, $subject, $message, $headers);
         echo "Спасибо за Ваше сообщение";
         }
