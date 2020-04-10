08aebcda23fd971f7cf1dd10100904fadd625a4d
exzolink <exzolink@gmail.com>
2020-04-11 00:40:17 +0300

    upd

---
+++ portfolio/clean/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+$('.content__inner_item').click(function () {
+    let id = $(this).attr('id');
+    $('.content__inner_item-desc').removeClass('visible');
+    $('.content-info').find('#' + id).addClass('visible');
+  });
+
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode === 27) {
+    $('.content__inner_item-desc').removeClass('visible');
+    }
+  });
+
+  $(document).click(function (e) {
+    if (!$(e.target).closest('.content__inner_item, .content__inner_item-desc').length) {
+      $(".content__inner_item-desc").removeClass("visible");
+    }
+  });
+
+});
