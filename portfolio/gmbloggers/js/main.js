d6931163926941254b5d2512423991d35bff0abf
exzolink <exzolink@gmail.com>
2020-07-02 01:05:02 +0300

    upd

---
+++ portfolio/gmbloggers/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+document.addEventListener('lazybeforeunveil', function(e){
+    var bg = e.target.getAttribute('data-bg');
+    if(bg){
+        e.target.style.backgroundImage = 'url(' + bg + ')';
+    }
+});
