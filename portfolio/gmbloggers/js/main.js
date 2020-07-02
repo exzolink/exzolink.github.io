45434702d820392d17c2f1d617ea44bbc1f09de1
exzolink <exzolink@gmail.com>
2020-07-02 03:04:03 +0300

    upd

--- portfolio/gmbloggers/js/main.js
+++ portfolio/gmbloggers/js/main.js
@@ -6,3 +6,28 @@ document.addEventListener('lazybeforeunveil', function(e){
         e.target.style.backgroundImage = 'url(' + bg + ')';
     }
 });
+
+function submitHandler(e) {
+    e.preventDefault();
+  
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() { 
+      console.log("readyState=", this.readyState, "statis=", this.status);
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+          console.log("SUCCESS", this);
+      }
+    }
+    
+    request.open(this.method, this.action, true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    
+    var data = new FormData(this);
+    for (var key of data.keys())
+      console.log(key, data.get(key));
+      
+    request.send(data);
+  }
+  
+  document.querySelectorAll("form").forEach(form =>
+    form.addEventListener("submit", submitHandler)
+  );
