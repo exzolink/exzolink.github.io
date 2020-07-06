740b19d14fcd1579a0b45170e40f1f427bca47a2
exzolink <exzolink@gmail.com>
2020-07-07 00:35:40 +0300

    upd

--- portfolio/gmbloggers/js/main.js
+++ portfolio/gmbloggers/js/main.js
@@ -77,40 +77,13 @@ window.onload = function () {
 };
 
 
-function submitHandler(e) {
-  e.preventDefault();
-
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function () {
-    console.log("readyState=", this.readyState, "statis=", this.status);
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-      console.log("SUCCESS", this);
-    }
-  }
-
-  request.open(this.method, this.action, true);
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-  var data = new FormData(this);
-  for (var key of data.keys())
-    console.log(key, data.get(key));
-
-  request.send(data);
-}
-
-document.querySelectorAll("form").forEach(form =>
-  form.addEventListener("submit", submitHandler)
-);
-
-
 var mySwiper = new Swiper('.swiper-container', {
   loop: true,
-  slidesPerView: 'auto',
+  slidesPerView: 1,
   freeMode: true,
   speed: 30000,
   onlyExternal: true,
   allowTouchMove: false,
-  preloadImages: false,
   simulateTouch: false,
   autoplay: {
     delay: 0
