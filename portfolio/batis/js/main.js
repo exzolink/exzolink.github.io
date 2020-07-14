33d10ffe137e3cd3495a533972f65bedf1440f1e
exzolink <exzolink@gmail.com>
2020-07-14 23:30:40 +0300

    upd

--- portfolio/batis/js/main.js
+++ portfolio/batis/js/main.js
@@ -6,3 +6,10 @@ $(document).ready(function () {
     };
 });
 
+document.addEventListener('lazybeforeunveil', function (e) {
+    var bg = e.target.getAttribute('data-bg');
+    if (bg) {
+      e.target.style.backgroundImage = 'url(' + bg + ')';
+    }
+  });
+
