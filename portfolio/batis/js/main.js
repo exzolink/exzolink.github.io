207b9cb0cc6eb24c01813dc5d6d402e17b162566
exzolink <exzolink@gmail.com>
2020-07-15 22:49:41 +0300

    Update main.js

--- portfolio/batis/js/main.js
+++ portfolio/batis/js/main.js
@@ -9,7 +9,9 @@ $(document).ready(function () {
 document.addEventListener('lazybeforeunveil', function (e) {
     var bg = e.target.getAttribute('data-bg');
     if (bg) {
-      e.target.style.backgroundImage = 'url(' + bg + ')';
+        e.target.style.backgroundImage = 'url(' + bg + ')';
     }
-  });
+});
 
+window.lazySizesConfig = window.lazySizesConfig || {};
+window.lazySizesConfig.loadMode = 1;
