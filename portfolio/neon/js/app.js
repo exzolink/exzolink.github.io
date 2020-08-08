b33ac2b995a0d05aab5f54fd6fb4eaf8456cf1c0
exzolink <exzolink@gmail.com>
2020-08-08 04:06:59 +0300

    Upd

--- portfolio/neon/js/app.js
+++ portfolio/neon/js/app.js
@@ -313,10 +313,10 @@ Spruce.store('modalPrivacy', {
     open: 'false',
 });
 
-var vh = window.innerHeight * 0.01;
+let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 window.addEventListener('resize', () => {
-    var vh = window.innerHeight * 0.01;
+    let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   });
