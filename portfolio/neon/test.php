1bda9d3fefb95a408e417f2c501e1cca3c663b8c
exzolink <exzolink@gmail.com>
2020-08-15 02:33:31 +0300

    Ajax for forms

--- portfolio/neon/test.php
+++ portfolio/neon/test.php
@@ -1,9 +1,9 @@
 <?php
-$to = "exzolink@gmail.com";
-$tema = "Заявка с сайта";
+$to = "test@gmail.com";
+$topic = "Заявка с сайта";
 $message .= "Имя: ".$_POST['name']."<br>";
-$message .= "Номер телефона: ".$_POST['email']."<br>";
+$message .= "Почта: ".$_POST['email']."<br>";
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-$verify = mail($to, $tema, $message, $headers); 
+$send = mail($to, $topic, $message, $headers); 
 ?>
