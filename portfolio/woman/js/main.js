ac4a3218bfcbddeea321f61b6cd68d3d31dd8e32
exzolink <exzolink@gmail.com>
2020-09-18 01:46:01 +0300

    Update

--- portfolio/woman/js/main.js
+++ portfolio/woman/js/main.js
@@ -4,3 +4,49 @@ let font = document.getElementById("font");
 window.addEventListener("load", function () {
 	font.rel = "stylesheet";
 });
+
+let posts = new Swiper(".swiper-container", {
+	slidesPerView: 3,
+	centeredSlides: true,
+	breakpoints: {
+		320: {
+			slidesPerView: 2,
+			spaceBetween: 10,
+			centeredSlides: true,
+			loop: true,
+		},
+		450: {
+			slidesPerView: 2,
+			spaceBetween: 15,
+			centeredSlides: true,
+			loop: true,
+		},
+		650: {
+			slidesPerView: 2,
+			spaceBetween: 25,
+			centeredSlides: true,
+			loop: true,
+		},
+		750: {
+			slidesPerView: 2,
+			spaceBetween: 30,
+			centeredSlides: true,
+			loop: true,
+		},
+		1140: {
+			slidesPerView: 3,
+			centeredSlides: false,
+			allowTouchMove: false,
+			simulateTouch: false,
+		},
+	},
+});
+
+const burgerBtn = document.querySelectorAll(".js-toggleBurger");
+const mobileMenu = document.querySelector(".header__menu");
+burgerBtn.forEach(function (e) {
+	e.addEventListener("click", function () {
+		mobileMenu.classList.toggle("active");
+		document.documentElement.classList.toggle('no-overflow');
+	});
+});
