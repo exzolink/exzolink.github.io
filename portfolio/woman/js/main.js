36d53c722663e1e726bd9b2e5c09f049d48f9a36
exzolink <exzolink@gmail.com>
2020-09-25 04:37:52 +0300

    Update

--- portfolio/woman/js/main.js
+++ portfolio/woman/js/main.js
@@ -1,20 +1,56 @@
 "use strict";
 
-let toPreload = document.querySelectorAll(".preload");
-window.addEventListener("load", function () {
-	for (let i = 0; i < toPreload.length; i++) {
-		toPreload[i].rel = "stylesheet";
-	}
-});
-
 // Фикс попапов на мобильных устройствах
-var vh = window.innerHeight * 0.01;
+let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 
 if (window.innerWidth < 800) {
 	window.addEventListener("resize", () => {
-		var vh = window.innerHeight * 0.01;
+		let vh = window.innerHeight * 0.01;
 		document.documentElement.style.setProperty("--vh", `${vh}px`);
+	});
+}
+
+const mainpageBtns = document.querySelectorAll(".main__item_btn");
+const mainpageLeftCloud = document.querySelector(".main__bg_left");
+const mainpageRightCloud = document.querySelector(".main__bg_right");
+const mainpageLeftBg = document.querySelector(".main__item_img.left");
+const mainpageCenterBg = document.querySelector(".main__item_img.center");
+const mainpageRightBg = document.querySelector(".main__item_img.right");
+if (window.innerWidth > 1100) {
+	mainpageBtns.forEach(function (item) {
+		item.addEventListener("mouseover", function () {
+			let itemId = item.getAttribute("data-btn");
+			switch (itemId) {
+				case "left":
+					mainpageLeftBg.style.transform = "translateX(20%) scale(1.2)";
+					mainpageCenterBg.style.transform = "translate(8%, 15%)";
+					mainpageLeftCloud.style.transform = "translate(-10%, 4%)";
+					mainpageRightCloud.style.transform = "translate(-10%, 4%)";
+					break;
+				case "center":
+					mainpageCenterBg.style.transform = "translateY(0%) scale(1.1)";
+					mainpageLeftBg.style.transform = "translateX(-5%)";
+					mainpageRightBg.style.transform = "translateX(5%)";
+					mainpageLeftCloud.style.transform = "translate(0%, -8%)";
+					mainpageRightCloud.style.transform = "translate(0%, -8%)";
+					break;
+				case "right":
+					mainpageRightBg.style.transform = "translateX(-10%) scale(1.2)";
+					mainpageCenterBg.style.transform = "translate(-8%, 15%)";
+					mainpageLeftCloud.style.transform = "translate(10%, 4%)";
+					mainpageRightCloud.style.transform = "translate(10%, 4%)";
+					break;
+			}
+		});
+
+		item.addEventListener("mouseleave", function () {
+			mainpageLeftBg.style.transform = "translateX(10%) scale(1)";
+			mainpageCenterBg.style.transform = "translate(0%, 10%) scale(1)";
+			mainpageRightBg.style.transform = "translate(0%, 0%) scale(1)";
+			mainpageLeftCloud.style.transform = "translate(0%, 0%)";
+			mainpageRightCloud.style.transform = "translate(0%, 0%)";
+		});
 	});
 }
 
@@ -31,7 +67,12 @@ if (document.getElementById("animated") !== null) {
 
 		let yPos = window.pageYOffset / 6;
 
-		let bgCoords = "translate(" + (animBgItem.dataset.velocity - (yPos / animBgItem.dataset.speed)) + "%," + (yPos / animBgItem.dataset.speed * 1.5) + "%)";
+		let bgCoords =
+			"translate(" +
+			(animBgItem.dataset.velocity - yPos / animBgItem.dataset.speed) +
+			"%," +
+			(yPos / animBgItem.dataset.speed) * 1.5 +
+			"%)";
 		animBgItem.style.transform = bgCoords;
 	}
 
