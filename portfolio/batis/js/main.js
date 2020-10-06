ae2e62201fbb65f28b7a346f1b528378c29c029d
exzolink <exzolink@gmail.com>
2020-10-06 18:32:05 +0300

    Update main.js

--- portfolio/batis/js/main.js
+++ portfolio/batis/js/main.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
-    if (window.innerWidth > 1100) {
-        $('#app').pagepiling({
-            scrollingSpeed: 200
-        });
-        window.lazySizesConfig = window.lazySizesConfig || {};
-        window.lazySizesConfig.loadMode = 1;
-    };
+	if (window.innerWidth > 1100) {
+		$("#app").pagepiling({
+			scrollingSpeed: 200,
+		});
+		window.lazySizesConfig = window.lazySizesConfig || {};
+		window.lazySizesConfig.loadMode = 1;
+	}
 });
 
-document.addEventListener('lazybeforeunveil', function (e) {
-    var bg = e.target.getAttribute('data-bg');
-    if (bg) {
-        e.target.style.backgroundImage = 'url(' + bg + ')';
-    }
+document.addEventListener("lazybeforeunveil", function (e) {
+	var bg = e.target.getAttribute("data-bg");
+	if (bg) {
+		e.target.style.backgroundImage = "url(" + bg + ")";
+	}
 });
 
+window.addEventListener("orientationchange", function () {
+    window.location.reload();
+}, false);
