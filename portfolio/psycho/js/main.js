d4656930d10fbffe130c74ac9423d10cded2222c
exzolink <exzolink@gmail.com>
2020-10-09 20:47:26 +0300

    Update

--- portfolio/psycho/js/main.js
+++ portfolio/psycho/js/main.js
@@ -1,8 +1,19 @@
-const menuBtn = document.querySelector(".header__burger");
+const menuBtn = document.querySelector(".header__burger img");
 const menu = document.querySelector(".header__menu");
+let htmlEl = document.documentElement;
 
-menuBtn.addEventListener("click", (e) => {
+menuBtn.addEventListener("click", () => {
+	htmlEl.classList.toggle("no-scroll");
+	console.log(htmlEl);
 	menu.classList.toggle("active");
+
+	if (menuBtn.classList.contains("opened")) {
+		menuBtn.classList.remove("opened");
+		menuBtn.src = "imgs/burger.svg";
+	} else {
+		menuBtn.classList.add("opened");
+		menuBtn.src = "imgs/close.svg";
+	}
 });
 
 if (document.querySelector(".mainpage__intro_video") !== null) {
@@ -30,7 +41,7 @@ if (document.querySelector(".mainpage__intro_video") !== null) {
 if (document.querySelector(".product") !== null) {
 	var productThumbs = new Swiper(".product__thumbs", {
 		slideToClickedSlide: true,
-		spaceBetween: 16,
+		spaceBetween: 20,
 		mousewheel: true,
 		loop: true,
 		loopAdditionalSlides: 2,
@@ -38,10 +49,25 @@ if (document.querySelector(".product") !== null) {
 		slidesPerSlide: 1,
 		breakpoints: {
 			320: {
-				slidesPerView: 3,
+				slidesPerView: 2,
+				navigation: {
+					nextEl: ".product__thumbs_next",
+					prevEl: ".product__thumbs_prev",
+				},
 			},
-			550: {
+			415: {
+				slidesPerView: 3,
+				navigation: {
+					nextEl: ".product__thumbs_next",
+					prevEl: ".product__thumbs_prev",
+				},
+			},
+			600: {
 				slidesPerView: 4,
+				navigation: {
+					nextEl: ".product__thumbs_next",
+					prevEl: ".product__thumbs_prev",
+				},
 			},
 			1500: {
 				direction: "vertical",
@@ -80,7 +106,6 @@ const basketBtns = document.querySelectorAll(
 const basketModal = document.querySelector(".modal-basket");
 const basketEl = document.querySelector(".modal-basket__block");
 const basketModalInner = document.querySelector(".modal-basket__inner");
-const htmlEl = document.documentElement;
 
 basketBtns.forEach((item) => {
 	item.addEventListener("click", () => {
