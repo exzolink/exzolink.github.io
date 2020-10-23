ebc80a9d32025ee0fd603bc9fef2f6891eb8e2b5
exzolink <exzolink@gmail.com>
2020-10-23 04:34:00 +0300

    New Project

--- portfolio/aksay/js/main.js
+++ portfolio/aksay/js/main.js
@@ -1,7 +1,33 @@
 "use strict";
 
-const videoWrapper = document.getElementById("video");
-window.addEventListener("load", () => {
-	videoWrapper.innerHTML =
-		"<video src='imgs/videoBg.mp4' muted autoplay loop alt=''></video>";
+const videoWrapper = document.querySelectorAll("#video");
+videoWrapper.forEach(item => {
+	window.addEventListener("load", () => {
+		item.innerHTML +=
+			"<video src='imgs/videoBg.mp4' muted loop alt=''></video>";
+	});
+})
+
+
+const slider_img = new Swiper(".slider-img__container", {
+	slidesPerView: 1,
+	loop: true,
+	loopedSlides: 1,
+	allowTouchMove: false,
+	simulateTouch: false,
 });
+
+const slider_text = new Swiper(".slider-text__container", {
+	slidesPerView: 1,
+	loop: true,
+	loopedSlides: 1,
+	allowTouchMove: false,
+	simulateTouch: false,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true
+	}
+});
+
+slider_img.controller.control = slider_text;
+slider_text.controller.control = slider_img;
