e6d757a393cb8e0a6dafbdebc2a62cb09de2b6ff
exzolink <exzolink@gmail.com>
2020-10-27 05:17:49 +0300

    Update

--- portfolio/gusi/js/main.js
+++ portfolio/gusi/js/main.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const mainSlider = new Swiper(".main-slider__container", {
-    slidesPerView: 1,
-    loop: true,
+	slidesPerView: 1,
+	loop: true,
 	pagination: {
 		el: ".swiper-pagination",
 		clickable: true,
@@ -10,11 +10,63 @@ const mainSlider = new Swiper(".main-slider__container", {
 });
 
 const sliders = new Swiper(".sliders__container", {
-    slidesPerView: 4,
-    slidesPerSlide: 2,
-    spaceBetween: 20,
+	slidesPerSlide: 2,
+	spaceBetween: 20,
 	pagination: {
 		el: ".swiper-pagination",
 		clickable: true,
 	},
+	breakpoints: {
+		320: {
+			slidesPerView: 2,
+			spaceBetween: 8,
+		},
+		760: {
+			slidesPerView: 3,
+			spaceBetween: 15,
+		},
+		900: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+		1150: {
+			slidesPerView: 4,
+			spaceBetween: 20,
+		},
+	},
 });
+
+
+const sliderNews = new Swiper(".sliders__news_container", {
+	slidesPerSlide: 2,
+	spaceBetween: 20,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 'auto',
+			spaceBetween: 10,
+			slidesOffsetBefore: 15,
+		},
+		500: {
+			slidesPerView: 2,
+			spaceBetween: 8,
+			slidesOffsetBefore: 0,
+		},
+		760: {
+			slidesPerView: 3,
+			spaceBetween: 15,
+		},
+		900: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+		1150: {
+			slidesPerView: 4,
+			spaceBetween: 20,
+		},
+	},
+});
+
