06e64b215250b034859faa25983c66fa03044d1f
exzolink <exzolink@gmail.com>
2021-02-21 17:31:22 +0300

    Update

--- portfolio/gmbloggers/js/main.js
+++ portfolio/gmbloggers/js/main.js
@@ -10,10 +10,9 @@ document.addEventListener("lazybeforeunveil", function (e) {
 var closePrivacy = document.getElementById("close-privacy");
 var openPrivacy = document.getElementById("privacy");
 var Privacy = document.querySelector(".privacy");
-
 var closeCall = document.getElementById("close-call");
 var closeConsult = document.getElementById("close-consult");
-
+var closeStats = document.getElementById("close-stats");
 var openPopup = document.querySelectorAll(
 	".blogers__btn_price, .blogers__btn_call, .nav__order, .footer__button",
 );
@@ -22,6 +21,11 @@ var consultPopup = document.getElementById("consult-popup");
 var openConsult = document.querySelectorAll(".header__order_btn");
 var Popup = document.querySelectorAll(".popup");
 var formSource = document.querySelector("#source");
+var resultsBlock = document.querySelector(".results");
+var statsPopup = document.querySelector("#stats-popup");
+var statsSlider = document.querySelectorAll(".stats-popup__block");
+var statsSliderContainer = document.querySelectorAll(".stats-popup__container");
+var statsBtns = document.querySelectorAll(".results__slide_button");
 
 closePrivacy.onclick = function () {
 	Privacy.classList.toggle("open");
@@ -147,8 +151,8 @@ var resultsSliders = new Swiper(".results__slider", {
 	observer: true,
 	observeParents: true,
 	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
+		nextEl: ".next-results",
+		prevEl: ".prev-results",
 	},
 	breakpoints: {
 		320: {
@@ -190,3 +194,57 @@ resultsTabs.forEach((tab) => {
 		tab.classList.add("active");
 	});
 });
+
+resultsBlock.addEventListener(
+	"mouseover",
+	async function () {
+		await fetch("./statsPopup.html")
+			.then((response) => response.text())
+			.then((html) => {
+				statsPopup.innerHTML = html;
+			});
+
+		var statsSliders = new Swiper(".stats-popup__container", {
+			slidesPerView: "auto",
+			centeredSlides: true,
+			autoHeight: true,
+			observer: true,
+			observeParents: true,
+			spaceBetween: 40,
+			navigation: {
+				nextEl: ".next",
+				prevEl: ".prev",
+			},
+		});
+
+		statsBtns.forEach(function (btn) {
+			btn.addEventListener("click", function () {
+				const blogerId = btn.closest(".results__slide").dataset.bloger;
+				if (!blogerId) return;
+
+				statsPopup.classList.add("open");
+				for (var i = 0; i < statsSlider.length; i++) {
+					statsSlider[i].classList.remove("active");
+				}
+				document
+					.querySelector(`.stats-popup__block[data-bloger="${blogerId}"]`)
+					.classList.add("active");
+
+				setTimeout(() => {
+					var event = new Event("resize", { bubbles: true, cancelable: true });
+					document.documentElement.dispatchEvent(event);
+					for (var i = 0; i < statsSliders.length; i++) {
+						statsSliders[i].setTranslate(0);
+						statsSliders[i].slideTo(0);
+					}
+				}, 50);
+			});
+		});
+
+		closeStats.onclick = function (e) {
+			e.preventDefault();
+			statsPopup.classList.toggle("open");
+		};
+	},
+	{ once: true },
+);
