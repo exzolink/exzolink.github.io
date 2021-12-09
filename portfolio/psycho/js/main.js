d7a8aae69a3318844e38b9ad3cc4b47441b824b2
exzolink <exzolink@gmail.com>
2021-12-09 17:47:31 +0300

    Psycho Update

--- portfolio/psycho/js/main.js
+++ portfolio/psycho/js/main.js
@@ -2,12 +2,12 @@ const menuBtn = document.querySelector(".header__burger img");
 const menu = document.querySelector(".header__menu");
 let htmlEl = document.documentElement;
 
-const postMapClose = document.querySelector('.postMap__close');
-const postMapEl = document.querySelector('.postMap__wrapper')
+const postMapClose = document.querySelector(".postMap__close");
+const postMapEl = document.querySelector(".postMap__wrapper");
 if (postMapClose) {
-postMapClose.addEventListener('click', () => {
-	postMapEl.classList.remove('active');
-})
+	postMapClose.addEventListener("click", () => {
+		postMapEl.classList.remove("active");
+	});
 }
 
 menuBtn.addEventListener("click", () => {
@@ -20,7 +20,6 @@ menuBtn.addEventListener("click", () => {
 	} else {
 		menuBtn.classList.add("opened");
 		menuBtn.src = "imgs/BX.gif";
-
 	}
 });
 
@@ -109,9 +108,7 @@ if (document.querySelector(".product") !== null) {
 	customSelect(".product__selector");
 }
 
-const basketBtns = document.querySelectorAll(
-	".header__basket, .modal-basket__close",
-);
+const basketBtns = document.querySelectorAll(".header__basket, .modal-basket__close");
 const basketModal = document.querySelector(".modal-basket");
 const basketEl = document.querySelector(".modal-basket__block");
 const basketModalInner = document.querySelector(".modal-basket__inner");
@@ -124,22 +121,6 @@ basketBtns.forEach((item) => {
 	});
 });
 
-document.addEventListener("keydown", (e) => {
-	if (e.keyCode === 27) {
-		basketModal.classList.remove("active");
-		basketEl.classList.remove("active");
-		htmlEl.classList.remove("no-overflow");
-	}
-});
-
-document.addEventListener("click", (e) => {
-	if (!e.target.closest(".modal-basket__block, .header__basket")) {
-		basketModal.classList.remove("active");
-		basketEl.classList.remove("active");
-		htmlEl.classList.remove("no-overflow");
-	}
-});
-
 try {
 	const alertEl = document.querySelector(".alerts__block");
 	alertEl.addEventListener("click", (e) => {
@@ -147,9 +128,7 @@ try {
 	});
 } catch (err) {}
 
-const checkoutAlert = document.getElementsByClassName(
-	"woocommerce-NoticeGroup-checkout",
-);
+const checkoutAlert = document.getElementsByClassName("woocommerce-NoticeGroup-checkout");
 
 for (let i = 0; i < checkoutAlert.length; i++) {
 	checkoutAlert[i].addEventListener("click", (e) => {
@@ -158,23 +137,76 @@ for (let i = 0; i < checkoutAlert.length; i++) {
 }
 
 document.addEventListener("click", (e) => {
-	let checkoutAlert = document.querySelector(
-		".woocommerce-NoticeGroup-checkout",
-	);
-	console.log(e.target);
+	let checkoutAlert = document.querySelector(".woocommerce-NoticeGroup-checkout");
 	if (e.target === checkoutAlert) {
 		checkoutAlert.remove();
-		return;
 	}
 });
 
-const aboutMore = document.querySelector('.about__more');
+let aboutMore = document.querySelector(".about__more");
 if (aboutMore) {
-	aboutMore.addEventListener('click', () => {
-		let aboutHiddenEls = document.querySelectorAll('.about__ul');
+	aboutMore.addEventListener("click", () => {
+		let aboutHiddenEls = document.querySelectorAll(".about__ul");
 		for (let i = 0; i < aboutHiddenEls.length; i++) {
-			aboutHiddenEls[i].classList.remove('about__ul');
+			aboutHiddenEls[i].classList.remove("about__ul");
 		}
-		aboutMore.closest('.about__more_wrapper').remove();
-	})
+		aboutMore.closest(".about__more_wrapper").remove();
+	});
 }
+
+const authBtn = document.getElementById("auth-btn");
+const authModal = document.getElementById("auth-modal");
+const authOverlay = document.querySelector(".page__overlay");
+if (authBtn && !authBtn.hasAttribute("href")) {
+	authBtn.addEventListener("click", () => {
+		authModal.classList.toggle("active");
+		authOverlay.classList.toggle("active");
+	});
+}
+
+const viewOrderBtn = document.querySelectorAll(".profile__button_more");
+viewOrderBtn.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		const orderItem = btn.closest(".profile__order");
+		const orderList = orderItem.querySelector(".profile__order_list");
+		orderList.classList.toggle("active");
+		btn.classList.toggle("active");
+	});
+});
+
+const editInfoBtn = document.getElementById("button-info");
+const editAddressBtn = document.getElementById("button-address");
+const infoModal = document.getElementById("info-modal");
+const addressModal = document.getElementById("address-modal");
+if (editInfoBtn && editAddressBtn) {
+	editInfoBtn.addEventListener("click", () => {
+		infoModal.classList.toggle("active");
+	});
+	editAddressBtn.addEventListener("click", () => {
+		addressModal.classList.toggle("active");
+	});
+}
+
+document.addEventListener("keydown", (e) => {
+	if (e.keyCode === 27) {
+		basketModal.classList.remove("active");
+		basketEl.classList.remove("active");
+		htmlEl.classList.remove("no-overflow");
+		authOverlay.classList.remove("active");
+		authModal.classList.remove("active");
+		infoModal.classList.remove("active");
+		addressModal.classList.remove("active");
+	}
+});
+
+document.addEventListener("click", (e) => {
+	if (!e.target.closest(".modal-basket__block, .header__basket, #auth-btn, #auth-modal, .profile__modal .auth__modal, .profile__meta_edit")) {
+		basketModal.classList.remove("active");
+		basketEl.classList.remove("active");
+		htmlEl.classList.remove("no-overflow");
+		authOverlay.classList.remove("active");
+		authModal.classList.remove("active");
+		infoModal.classList.remove("active");
+		addressModal.classList.remove("active");
+	}
+});
